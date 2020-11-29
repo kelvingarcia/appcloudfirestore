1289fbbde568a07b22a9713748e55a035defa72f
Kelvin Garcia <kelvinrgarcia98@gmail.com>
2020-11-29 15:17:57 -0300

    corrigindo validações

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -53,7 +53,7 @@ class _LoginState extends State<Login> {
     Pattern pattern =
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     RegExp regex = new RegExp(pattern);
-    return (!regex.hasMatch(value)) ? false : true;
+    return (regex.hasMatch(value)) ? false : true;
   }
 
   @override
--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -65,7 +65,7 @@ class _SignUpState extends State<SignUp> {
     Pattern pattern =
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     RegExp regex = new RegExp(pattern);
-    return (!regex.hasMatch(value)) ? false : true;
+    return (regex.hasMatch(value)) ? false : true;
   }
 
   @override
